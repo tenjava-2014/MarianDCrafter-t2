576b8b9855b84eb07190b449f27515f817689100
MarianDCrafter <marian@joetabu.de>
2014-07-07 14:17:07 +0200

    Committing and Pushing works!

--- src/main/java/com/tenjava/entries/MarianDCrafter/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/MarianDCrafter/t2/TenJava.java
@@ -2,4 +2,4 @@ package com.tenjava.entries.MarianDCrafter.t2;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {} // Committing and Pushing works!
